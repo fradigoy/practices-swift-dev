84daf8ce1b6b926a9b4c85a2120e3478cae16808
blacksmash <frederic.radigoy@icloud.com>
2017-03-27 14:40:53 +0200

    commit 2 Tip Calculator

--- tipCalculor/tipCalculor/ViewController.swift
+++ tipCalculor/tipCalculor/ViewController.swift
@@ -8,17 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var pourcentagePourBoire:Float?
+    
+    @IBOutlet weak var montantFactureTextField: UITextField!
+    
+    @IBOutlet weak var pourBoireSegmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var pourBoireLabel: UILabel!
+    @IBOutlet weak var montantTotalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pourBoireSegmentControl.addTarget(self, action: #selector(ViewController.calculerPourCentage), for: UIControlEvents.valueChanged)
+        
+        montantFactureTextField.delegate = self
+        montantFactureTextField.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Functions
+    
+    
+    
+    func calculerPourCentage(){
+        
+        switch pourBoireSegmentControl.selectedSegmentIndex {
+        case 0:
+            print("segment sélectionné: \(pourBoireSegmentControl.selectedSegmentIndex)")
+            pourcentagePourBoire = 15
+            break
+        case 1:
+            print("segment sélectionné: \(pourBoireSegmentControl.selectedSegmentIndex)")
+            pourcentagePourBoire = 20
+            break
+        case 2:
+            print("segment sélectionné: \(pourBoireSegmentControl.selectedSegmentIndex)")
+            pourcentagePourBoire = 25
+            break
+
+        default:
+            print("Pas de sélection")
+        }
+        calculerPourBoire(pourcent: Float(pourcentagePourBoire!))
     }
+    
+    func calculerPourBoire(pourcent:Float){
+        let pourBoire = Float(montantFactureTextField.text!)! * pourcent / 100
+        
+        let montant = Float(montantFactureTextField.text!)
+        let pourBoireFormatString = NSString(format: "%.2f", pourBoire)
+        let montantTotalFormatStr = NSString(format: "%.2f", pourBoire + montant!)
+        
+        pourBoireLabel.text = "€  \(pourBoireFormatString)"
+        montantTotalLabel.text = "€  \(montantTotalFormatStr)"
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        montantFactureTextField.text = ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 
 
 }
